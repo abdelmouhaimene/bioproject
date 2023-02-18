c4e5b04e59ab5643452eb7fbe3b173385355ec2f
moha <mimou4ever1999@gmail.com>
2023-02-18 02:44:11 +0100

    first commit prepare containers and componentes

---
+++ src/containers/sections/Sections.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './sections.css'
+const Sections = () => {
+  return (
+    <div>Sections</div>
+  )
+}
+
+export default Sections
---
+++ src/containers/whatproject/WhatProject.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './whatProject.css'
+const WhatProject = () => {
+  return (
+    <div>WhatProject</div>
+  )
+}
+
+export default WhatProject
