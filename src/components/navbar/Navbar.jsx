f53e89dbd35c368f1ed3643358613b8a2ab2b55e
moha <mimou4ever1999@gmail.com>
2023-02-20 03:05:29 +0100

    3rd commit complet nav bar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,16 +1,46 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './navbar.css'
-import {RiMenu3Line, RiCloseLin} from 'react-icons/ri'
+import {RiMenu3Line, RiCloseLine} from 'react-icons/ri'
+import logo from '../../assets/walogo1.svg'
+
+const Menu = () => (
+  <>
+    <p><a href='#home'>Accueil</a></p>
+    <p><a href='#about'>Nous</a></p>
+    <p><a href='#product'>Produits</a></p>
+    <p><a href='#buy'>Acheter</a></p>
+    <p><a href='#blog'>bibliotheque</a></p>
+  </>
+)
+
 const Navbar = () => {
+  const [toggleMenu , setToggleMenu] = useState(false) ;
   return (
     <div className="navbar">
       <div className='navbar-links'>
         <div className='navbar-links_logo'>
-
+          <img src={logo} alt="company logo" />
         </div>
-
+        <div className='navbar-links_container'>
+          <Menu />
+        </div>
       </div>
-
+      <div className='navbar-menu'>
+        {
+          toggleMenu
+          ? <RiCloseLine color='#fff' size={27} onClick={() => setToggleMenu(false)} />
+          : <RiMenu3Line color='#fff' size={27} onClick={() => setToggleMenu(true)} />
+        }
+        {
+          toggleMenu && (
+            <div className='navbar-menu_container scale-up-center'>
+              <div className='navbar-menu_container-links'>
+                <Menu />
+              </div>
+            </div>
+          )
+        }
+      </div>
     </div>
   )
 }
