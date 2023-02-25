785ad94ff4b54b86d99246e4b5b9dcb1ce1c6e73
moha <mimou4ever1999@gmail.com>
2023-02-25 02:08:13 +0100

    5th commit the first page look good

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import './brand.css'
 
+
 const Brand = () => {
   return (
-    <div>Brand</div>
+    <div className='brand-container'>
+      <div className='brand-container_img facebook'>
+        <i class="fa-brands fa-facebook-f"></i>
+        <a href='https://www.facebook.com'><span>Facebook</span></a>
+      </div>
+      <div className='brand-container_img instagram'>
+      <i class="fa-brands fa-instagram"></i>
+      <a href='https://www.instagram.com'><span>Instagram</span></a>
+      </div>
+      <div className='brand-container_img gmail'>
+        <i class="fa-brands fa-google"></i>
+        <a href='https://www.gmail.com'><span>Gmail</span></a>
+      </div>
+      <div className='brand-container_img whatsapp'>
+        <i class="fa-brands fa-whatsapp"></i>
+        <a href='https://www.whatsapp.com'><span>Whatsapp</span></a>
+      </div>
+      
+
+    </div>
   )
 }
 
--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -6,8 +6,20 @@ const Header = () => {
   return (
     <div className='_header section_padding' id='home'>
       <div className='_header-content'>
-        <h1 className='gradient_text'>Bienvenue dans notre boutique en ligne chez Assila</h1>
-        <p>Notre magasin propose des services de vente et de livraison d'arbres prêts à planter, de miel naturel et d'huile d'olive</p>
+        <h1>
+          Bienvenue dans notre boutique en ligne chez Assila
+        </h1>
+        <p>
+          Notre magasin propose des services de vente et de livraison d'arbres prêts à planter,
+          de miel naturel et d'huile d'olive.
+        </p>
+        <p>
+          Vous etes pret? allez faire une commande ! 
+        </p>
+        <div className='_header-content_form'>
+          <input type="email" placeholder='Entrez émail ici !'/>
+          <button type='button'>Commencer</button>
+        </div>
         <div className='_header-content_people'>
           <img src={people} alt=" a people pic" />
           <p>+1.6k visiteurs</p>
